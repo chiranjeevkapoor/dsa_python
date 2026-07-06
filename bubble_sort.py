7538ca72a39625c58208a93e95e90505ac7e972b
Chiranjeev Kapoor <kapoorchiranjeev@gmail.com>
2026-07-07 02:56:51 +0530

    bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,16 @@
+class Solution:
+    def bubbleSort(self,arr):
+        n = len(arr)
+        
+        for i in range(n-1, -1, -1):
+            swapped=False
+            for j in range(i):
+                if arr[j]>arr[j+1]:
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+                    swapped = True
+            
+            if not swapped:
+                break
+        
+        return arr
+                
