ca66e157beca923812554db5488901c9942c52f3
Chiranjeev Kapoor <kapoorchiranjeev@gmail.com>
2026-07-10 01:49:49 +0530

    optimized right

--- right_rotate_by_k.py
+++ right_rotate_by_k.py
@@ -13,4 +13,33 @@ class Solution:
         
         for i in range(k):
             nums[i] = temp[i]
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # n = len(nums)
+        # k = k%n
+
+        # temp = nums[n-k:n]
+
+        # for i in range(n-k-1, -1, -1):
+        #     nums[i+k] = nums[i]
+        
+        # for i in range(k):
+        #     nums[i] = temp[i]
+
+        n = len(nums)
+        k = k%n
+
+        def reverse(l, r):
+            while l<r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l+=1
+                r-=1
+        
+        reverse(0, n-k-1)
+        reverse(n-k, n-1)
+        reverse(0, n-1)
         
