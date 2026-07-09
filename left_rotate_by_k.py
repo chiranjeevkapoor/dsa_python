e547e8025bac1e698c85e3bfdb20c7b3381f4fef
Chiranjeev Kapoor <kapoorchiranjeev@gmail.com>
2026-07-10 01:42:11 +0530

    optimized left rotate

--- left_rotate_by_k.py
+++ left_rotate_by_k.py
@@ -8,3 +8,22 @@ def rotateArray(arr: list, k: int) -> list:
     for i in range(n-k, n):
         arr[i] = temp[i - (n-k)]
     return arr
+
+
+
+class Solution:
+    def rotateArr(self, arr, d):
+        #code here
+        n = len(arr)
+        d = d%n
+        
+        def reverse(l, r):
+            while l<r:
+                arr[l], arr[r] = arr[r], arr[l]
+                l+=1
+                r-=1
+        
+        reverse(0, d-1)
+        reverse(d, n-1)
+        reverse(0, n-1)
+        
